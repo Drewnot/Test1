6c39a878d40f2db08c584ab2114169530928be60
Andy <andrewnyawira54@gmail.com>
2025-11-26 13:47:46 +0300

    Uploading file from bash

---
+++ Java files/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        /*String myname = "John Doe";
+        String myjob = "Software Engineer";
+        
+        System.out.println(myname);*/
+
+        int first_num = 5, second_num = 10, sum;
+
+        sum = first_num + second_num;
+        System.out.println("The sum is: " +sum);
+    }
+}
